6fa229aac7593947b5485e7a1a58a378d49b45fa
Thomas Buchberger <t.buchberger@4teamwork.ch>
2020-10-19 11:33:03 +0200

    Add logging and improvements from sablon

--- msgconvert.py
+++ msgconvert.py
@@ -7,75 +7,91 @@ It expects a multipart/form-data upload containing a .msg file with the name
 msg and returns the converted eml file.
 """
 from aiohttp import web
-from tempfile import mkdtemp
+import tempfile
 import os.path
 import subprocess
-import shutil
+import logging
 
 CHUNK_SIZE = 65536
+
+logger = logging.getLogger('msgconvert')
 
 
 async def msgconvert(request):
 
-    filename = None
+    form_data = {}
     temp_dir = None
 
     if not request.content_type == 'multipart/form-data':
+        logger.info(
+            'Bad request. Received content type %s instead of multipart/form-data.',
+            request.content_type,
+        )
         return web.Response(status=400, text=f"Multipart request required.")
 
     reader = await request.multipart()
-    while True:
-        part = await reader.next()
 
-        if part is None:
-            break
+    with tempfile.TemporaryDirectory() as temp_dir:
+        while True:
+            part = await reader.next()
 
-        if part.name == 'msg':
-            temp_dir = mkdtemp()
-            filename = os.path.join(temp_dir, part.filename)
-            with open(os.path.join(temp_dir, filename), 'wb') as msg_file:
-                while True:
-                    chunk = await part.read_chunk(CHUNK_SIZE)
-                    if not chunk:
-                        break
-                    msg_file.write(chunk)
+            if part is None:
+                break
 
-    if filename is not None:
-        outfilename = os.path.join(
-            os.path.dirname(filename), os.path.basename(filename) + '.eml')
-        res = subprocess.run(
-            ['msgconvert', '--outfile', outfilename, filename],
-            capture_output=True,
-            text=True,
-        )
+            if part.name == 'msg':
+                form_data['msg'] = await save_part_to_file(part, temp_dir)
 
-        if res.returncode == 0:
-            response = web.StreamResponse(
-                status=200,
-                reason='OK',
-                headers={'Content-Type': 'message/rfc822'},
+        if 'msg' in form_data:
+            outfilename = os.path.join(
+                temp_dir, os.path.basename(form_data['msg']) + '.eml')
+            res = subprocess.run(
+                ['msgconvert', '--outfile', outfilename, form_data['msg']],
+                capture_output=True,
+                text=True,
             )
-            await response.prepare(request)
 
-            with open(outfilename, 'rb') as outfile:
-                while True:
-                    data = outfile.read(CHUNK_SIZE)
-                    if not data:
-                        break
-                    await response.write(data)
+            if res.returncode == 0:
+                response = web.StreamResponse(
+                    status=200,
+                    reason='OK',
+                    headers={'Content-Type': 'message/rfc822'},
+                )
+                await response.prepare(request)
 
-            await response.write_eof()
-            shutil.rmtree(temp_dir)
-            return response
-        else:
-            shutil.rmtree(temp_dir)
-            return web.Response(
-                status=500, text=f"Conversion failed. {res.stderr}")
+                with open(outfilename, 'rb') as outfile:
+                    while True:
+                        data = outfile.read(CHUNK_SIZE)
+                        if not data:
+                            break
+                        await response.write(data)
 
+                await response.write_eof()
+                return response
+            else:
+                logger.error('Conversion failed. %s', res.stderr)
+                return web.Response(
+                    status=500, text=f"Conversion failed. {res.stderr}")
+
+    logger.info('Bad request. No msg provided.')
     return web.Response(status=400, text=f"No msg provided.")
 
 
+async def save_part_to_file(part, directory):
+    filename = os.path.join(directory, part.filename)
+    with open(os.path.join(directory, filename), 'wb') as file_:
+        while True:
+            chunk = await part.read_chunk(CHUNK_SIZE)
+            if not chunk:
+                break
+            file_.write(chunk)
+    return filename
+
+
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(name)s %(message)s',
+        level=logging.INFO,
+    )
     app = web.Application()
     app.add_routes([web.post('/', msgconvert)])
     web.run_app(app)
